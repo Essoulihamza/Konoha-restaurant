0801c3c5ed3176fefda1f96964a8fa91680eefe2
Essoulihamza <ehamza.web@gmail.com>
2022-10-30 21:04:31 +0100

    added js file

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+let burgerMenu = document.getElementById('burger');
+let nav = document.getElementById('nav');
+
+
+burgerMenu.addEventListener('click', function(){
+	nav.classList.toggle('show');
+});
