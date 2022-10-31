15adc8f6e2cada4f5a46f555c4f0e9da12dd240a
EssouliHamza <ehamza.web@gmail.com>
2022-10-31 19:14:58 +0100

    last commit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,29 @@
 let burgerMenu = document.getElementById('burger');
-let nav = document.getElementById('nav');
-
+let navBg = document.querySelector('#nav-bg');
+let nav = document.querySelector('#nav');
 
 burgerMenu.addEventListener('click', function(){
-	nav.classList.toggle('show');
+	nav.classList.add('show');
+	navBg.classList.add('display');
 });
+navBg.addEventListener('click', function(){
+	nav.classList.remove('show');
+	navBg.classList.remove('display');
+});
+window.onscroll = () => {
+	nav.classList.remove('show');
+	navBg.classList.remove('display');
+	cart.classList.remove('showed');
+}
+/__________________________________________________________________________________________/
+let cart = document.getElementById('cart-bx');
+let shopIcon = document.getElementsByClassName('shoping-cart')[0];
+
+shopIcon.addEventListener('click', function(){
+	cart.classList.toggle('showed');
+})
+
+let test = document.querySelector('.plat-1');
+console.log(test);
+test.classList.remove('all');
+
